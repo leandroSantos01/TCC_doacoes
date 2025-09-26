fd0d3b40f692e3b0bc92e8ea5f3b603f64240d87
KRAT00S1 <ra54029168884@acaonsfatima.org.br>
2025-09-26 19:19:51 -0300

    inicio

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -5,6 +5,7 @@ import '/src/scss/global.scss'
 import '/src/scss/fonts.scss'
 
 import logo from '/src/assets/images/logos.png'
+import caixa from '/src/assets/images/caixa.jpg'
 
 
 
@@ -29,15 +30,19 @@ export default function App(){
 
         </header>
 
-        <main className="container_central">
+        <main >
+
+        <div className="container_central">   
        
         <div className="container_img">
-            <img src="" alt="" />
+            <img src={caixa} />
         </div>
         <div className="container_infromacao">
-        <h1></h1>
+        <h1>O que  é a <span className='destaque'>DonNet?</span></h1>
 
         <p>
+        Somos um grupo movido pela <span className='destaque'>solidariedade</span> e pelo desejo genuíno de fazer a diferença. Acreditamos no poder dos gestos simples, capazes de transformar realidades e renovar esperanças.
+        Nossa plataforma nasceu para conectar pessoas que querem ajudar a causas que precisam ser ouvidas. Com ética, transparência e compromisso social, criamos um ambiente seguro onde cada doação se transforma em impacto positivo.
         
         </p>
 
@@ -45,27 +50,28 @@ export default function App(){
 
         </div>
 
-   
+        </div> 
 
 
 
         </main>
 
-        <main className="container_conteudo">
-            
-
-        <div className="conteudo">
-
-
-        </div>
-
-
-        </main >
-
-        
-
 
         <section >
+
+            <div className="container_conteudo">
+            <h1>Ajude e faça alguém mais feliz!</h1>
+
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere  varius dolor a vehicula. Donec aliquam dictum facilisis. Suspendisse  tincidunt neque id metus varius pretium. Praesent at dignissim quam.  Aliquam semper faucibus volutpat. In at scelerisque nisi. Etiam blandit  justo in tortor dictum mollis. In ut ligula egestas, tincidunt nisl nec, consectetur neque.
+            </p>
+
+            <Link className='doeAqui'>Doar</Link>
+
+            </div>
+        </section>
+
+         <section >
 
             <div className="carrossel">
 
