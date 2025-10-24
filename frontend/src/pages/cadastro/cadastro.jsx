e49ffe3a15aea953b025ecac5338057a5c0c821f
CookieCeleste <>
2025-10-24 08:00:39 -0300

    login api conectando

--- frontend/src/pages/cadastro/cadastro.jsx
+++ frontend/src/pages/cadastro/cadastro.jsx
@@ -1,47 +1,74 @@
 import Cabecalho from "../../components/cabecalho/Cabecalho.jsx";
+import { useState } from "react";
+import * as api from '../../api.js';
 import './cadastro.scss';
 
 export default function Cadastro(){
+
+    const [formData, setFormData] = useState({
+        email: '',
+        usuario: '',
+        senha: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const dataToSend = {
+            ...formData,
+        };
+        try {
+            await api.post('/login', dataToSend);
+            alert('Logado!');
+            console.log(formData);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
 return(
     <div>
 
         <Cabecalho/>
 
         <main>
-    
             <div className="container-mae">
-            
                 <div className="container-filha1">
+                    <form onSubmit={handleSubmit}>
 
-                    <h1>Cadastro</h1>
-                    <p>Venha fazer parte da nossa comunidade.</p>
+                        <h1>Cadastro</h1>
+                        <p>Venha fazer parte da nossa comunidade.</p>
 
-                    <div className="container-input">
-                        <label>Email</label>
-                        <input type="email" id="" />
-                    </div>
+                        <div className="container-input">
+                            <label>Email</label>
+                            <input type="email" placeholder='Email' name='email' value={formData.email} onChange={handleChange} />
+                        </div>
 
-                    <div className="container-input">
-                        <label>Usuario</label>
-                        <input type="text" name="" id="" />
-                    </div>
+                        <div className="container-input">
+                            <label>Usuário</label>
+                            <input type="text" placeholder='Usuário' name='usuario' value={formData.usuario} onChange={handleChange} />
+                        </div>
 
-                    <div className="container-input">
-                        <label>Senha</label>
-                        <input type="email" id="" />
-                    </div>
+                        <div className="container-input">
+                            <label>Senha</label>
+                            <input type="password" placeholder='Inserir senha' name='senha' value={formData.senha} onChange={handleChange} />
+                        </div>
 
-                    <div className="container-check">
-                        <input type="checkbox" name="" id="" />
-                        <label>Declaro que li e concordo com os <a href="">Termos de Uso.</a></label>
-                    </div>
+                        <div className="container-check">
+                            <input type="checkbox" name="" id="" />
+                            <label>Declaro que li e concordo com os <a href="">Termos de Uso.</a></label>
+                        </div>
 
-                    <button className="cadastrar-se">Cadastrar</button>
+                        <button type="submit" className="cadastrar-se">Cadastrar</button>
+                    </form>
 
                 </div>
 
                 <div className="container-filha2">
-
+                    
                 </div>
 
             </div>
