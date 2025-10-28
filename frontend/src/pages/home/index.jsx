285ea967db0e380a0e98a98b16c9d08e04b7d6e0
KRAT00S1 <ra54029168884@acaonsfatima.org.br>
2025-10-27 23:25:20 -0300

    update

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -1,12 +1,21 @@
 import { Link } from 'react-router'
 
+import './swiper.scss'
 import './index.scss'
 import '/src/scss/global.scss'
 import '/src/scss/fonts.scss'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
 import Cabecalho from '../../components/cabecalho/Cabecalho.jsx'
 import Rodape from '../../components/Rodape/Rodape.jsx'
 
-import caixa from '/src/assets/images/image.png'
+import informacao from '/src/assets/images/imageInfo.png'
+import cao from '/src/assets/images/cao.png'
+import doar from '/src/assets/images/doar.png'
+import caramelo from '/src/assets/images/caramelo.png'
+import crianca from '/src/assets/images/crianca.png'
+import amigos from '/src/assets/images/amigos.png'
 
 
 
@@ -33,7 +42,7 @@ export default function App() {
                     </div>
 
                     <div className="container_img">
-                        <img src={caixa} />
+                        <img src={informacao} />
                     </div>
 
 
@@ -52,20 +61,53 @@ export default function App() {
 
             <section >
 
-                <div className="container_conteudo">
-                    <h1>Ajude e faça alguém mais feliz!</h1>
 
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec posuere  varius dolor a vehicula. Donec aliquam dictum facilisis. Suspendisse  tincidunt neque id metus varius pretium. Praesent at dignissim quam.  Aliquam semper faucibus volutpat. In at scelerisque nisi. Etiam blandit  justo in tortor dictum mollis. In ut ligula egestas, tincidunt nisl nec, consectetur neque.
-                    </p>
+                <div className="carousel-container">
+                    <Swiper spaceBetween={20} slidesPerView={3} loop>
+                        <SwiperSlide>
+                            <div className="slide">
+                                <img src={cao} alt="Slide 1" />
+                                <h3>Praia paradisíaca</h3>
+                                <p>Um lugar perfeito para relaxar e aproveitar o sol.</p>
+                            </div>
+                        </SwiperSlide>
 
-                    <Link className='doeAqui'>Doar</Link>
+                        <SwiperSlide>
+                            <div className="slide">
+                                <img src={caramelo} alt="Slide 2" />
+                                <h3>Montanha nevada</h3>
+                                <p>Paisagem incrível para quem ama frio e aventura.</p>
+                            </div>
+                        </SwiperSlide>
 
+                        <SwiperSlide>
+                            <div className="slide">
+                                <img src={amigos} alt="Slide 3" />
+                                <h3>Cidade moderna</h3>
+                                <p>A mistura perfeita de cultura e tecnologia.</p>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="slide">
+                                <img src={doar} alt="Slide 4" />
+                                <h3>Cidade moderna</h3>
+                                <p>A mistura perfeita de cultura e tecnologia.</p>
+                            </div>
+                        </SwiperSlide>
+
+                        <SwiperSlide>
+                            <div className="slide">
+                                <img src={crianca} alt="Slide 5" />
+                                <h3>Cidade moderna</h3>
+                                <p>A mistura perfeita de cultura e tecnologia.</p>
+                            </div>
+                        </SwiperSlide>
+                    </Swiper>
                 </div>
+
+
             </section>
-
-
-
 
 
 
