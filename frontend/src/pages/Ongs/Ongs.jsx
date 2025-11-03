b8ddc4007c293be24e635c22c5af6badc6deffa3
KRAT00S1 <ra54029168884@acaonsfatima.org.br>
2025-11-03 08:54:34 -0300

    up

--- frontend/src/pages/Ongs/Ongs.jsx
+++ frontend/src/pages/Ongs/Ongs.jsx
@@ -5,14 +5,52 @@ import './ongs.scss'
 import '/src/scss/global.scss'
 import '/src/scss/fonts.scss'
 
+import { FaSearch } from "react-icons/fa";
+
+import api from "../../api"
+
 import { useState } from "react"
 import ModalOngs from "../../components/modalOng/Index"
 
 
 
 export default function Ongs() {
-    const [salvaongs,setSalvarongs] =useState()
+    const [salvaongs,setSalvarongs] =useState([])
     const [modalOngs, setModalOngs] = useState(false)
+    const [nome, setNome] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [cnpj, setCnpj] = useState("");
+    const [categoria, setCategoria] = useState("");
+  
+
+    async function Cadastrar() {
+      if(!nome|| !email || !senha || !endereco || !cnpj ||categoria){
+        alert('precisa de nome')
+      }
+
+       let body = {
+        "nome":nome,
+        "email":email,
+        "senha":senha,
+        "endereco":endereco,
+        "cnpj":cnpj,
+        "categoria":categoria
+       }
+       try {
+        const resp = await api.post("/cadastro/ong", body);
+        setSalvarongs([...salvaongs, resp.data]); // adiciona a nova ONG à lista
+        alert("ONG cadastrada com sucesso!");
+        setModalOngs(false);
+      } catch (e) {
+        alert(e.response?.data?.erro || "Erro ao cadastrar ONG.");
+      }
+    }
+      
+    
+      
+    
 
 
     return (
@@ -23,7 +61,14 @@ export default function Ongs() {
                 <div className="central_ongs">
 
                 <div className="btn">
-                    <button onClick={()=>setModalOngs(true)}>Cadastrar Ongs</button>
+                    <div className="pesquisa">
+                    <input type="text" placeholder="digite o nome ou categoria "   />
+                    <hr />
+                    <button className="btn_pesquisa"><FaSearch /></button>
+                    
+                    </div>
+
+                    <button className="cadastrar_ong" onClick={()=>setModalOngs(true)}>Cadastrar Ongs</button>
 
                 </div>
 
@@ -38,28 +83,28 @@ export default function Ongs() {
   aberto={modalOngs}
   titulo={'Cadastro de Ongs'}
   fechado={() => setModalOngs(false)}
-  cadastrar={'ola'}
+  cadastrar={Cadastrar}
   conteudo={
     <div>
         
       <div>
         <label>Nome</label>
-        <input placeholder='nome' type='text' />
+        <input placeholder='nome' type='text' value={nome} onChange={e => setNome(e.target.value)} />
       </div>
 
       <div>
         <label>Email</label>
-        <input placeholder='email' type='email' />
+        <input placeholder='email' type='email' value={email} onChange={e=> setEmail(e.target.value)}/>
       </div>
 
       <div>
         <label>Senha</label>
-        <input type="password" placeholder='senha' />
+        <input type="password" placeholder='senha' value={senha} onChange={e => setSenha(e.target.value)}/>
       </div>
 
        <div>
         <label>Endereço</label>
-        <input type="text" placeholder='endereço' />
+        <input type="text" placeholder='endereço' value={endereco} onChange={e => setEndereco(e.target.value)}/>
       </div>
 
 
@@ -71,11 +116,11 @@ conteudo2={
        
       <div>
         <label>CNPJ</label>
-        <input type="text" placeholder='CNPJ' />
+        <input type="text" placeholder='CNPJ' value={cnpj} onChange={e => setCnpj(e.target.value)}/>
       </div>
   <div>
         <label>Categoria</label>
-        <select>
+        <select value={categoria} onChange={e => setCategoria(e.target.value)}>
           <option disabled selected hidden>Selecione um tipo</option>
           <option>Ambiental</option>
           <option>Assistência social</option>
@@ -94,4 +139,5 @@ conteudo2={
 
         </div>
     )
+
 }
