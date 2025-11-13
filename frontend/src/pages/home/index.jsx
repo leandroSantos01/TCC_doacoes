bf849a12d15c7fe4c59ae209416c8802c1951922
Livh17 <aleguem2@gmail.com>
2025-11-13 08:44:42 -0300

    commit

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -128,7 +128,7 @@ export default function App() {
                             </div> */}
               <div className="slide">
                 <Link
-                  to="/caosemdono"
+                  to="/caoSemDono"
                   style={{ textDecoration: "none", color: "black" }}
                 >
                   <OngListada
